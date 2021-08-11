67b8cee9e5fa38decdf24e1558d307a3558e6950
情深骚明 <wmy_2000@163.com>
2021-08-11 13:32:54 +0800

    测试如何进行提交版本切换，以及重新创建分支

---
+++ src/main/java/wmy/flink/demo/sql/Test.java
@@ -0,0 +1,18 @@
+package wmy.flink.demo.sql;
+
+/**
+ * className: Test
+ * package: wmy.flink.demo.sql
+ *
+ * @date: 2021/8/12
+ * @author: 数仓开发工程师
+ * @email: wmy_2000@163.com
+ * @Company: 亚信技术有限公司
+ * @blog: https://wmybigdata-1.github.io/
+ * @Descirption:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试如何进行一个提交");
+    }
+}
