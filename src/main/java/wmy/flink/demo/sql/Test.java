887cf6926b7f061978977a0d025dbb97d84f194e
情深骚明 <wmy_2000@163.com>
2021-08-12 10:45:33 +0800

    测试提交回滚切换之后，在进行切换到原来的分支然后进行再一次提交

---
+++ src/main/java/wmy/flink/demo/sql/Test.java
@@ -0,0 +1,19 @@
+package wmy.flink.demo.sql;
+
+/**
+ * className: Test
+ * package: wmy.flink.demo.sql
+ *
+ * @date: 2021/8/12
+ * @author: 数仓开发工程师
+ * @email: wmy_2000@163.com
+ * @Company: 亚信技术有限公司
+ * @blog: https://wmybigdata-1.github.io/
+ * @Descirption:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试提交回滚切换之后，在进行切换到原来的分支然后进行再一次提交");
+    }
+}
+
